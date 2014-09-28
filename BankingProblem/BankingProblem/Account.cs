757c47904486376e18964b4ceba649dfd672fa8c
ZahidHasan4mJU <responsible.zahid@yahoo.com>
2014-09-28 12:22:54 +0600

    succeed

---
+++ BankingProblem/BankingProblem/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingProblem
+{
+    internal class Account
+    {
+        public string accNum;
+        public string custName;
+        public double balance = 0;
+
+        public double Deposit(double amount)
+        {
+            balance += amount;
+            return balance;
+        }
+
+        public double Withdraw(double amount)
+        {
+            balance -= amount;
+            return balance;
+        }
+        
+    
+}
+}
